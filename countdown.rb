f389f8b6c808eb3c22d609ef38dd22f58fbc7eac
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-03 18:15:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 
 def countdown(number)
-while number >= 0
+while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
 end
@@ -8,7 +8,7 @@ return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  while number >= 0
+  while number > 0
     puts "#{number} SECOND(S)!"
   number -= 1
   sleep 1
