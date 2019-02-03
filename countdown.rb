97b0eb63c10fffc22ca8e6efd9c0b6d09a1da84b
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-03 15:35:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,5 +8,12 @@ puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  
+  while number >= 0
+    puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 1
+end
+puts "HAPPY NEW YEAR!"
+end
+
 
